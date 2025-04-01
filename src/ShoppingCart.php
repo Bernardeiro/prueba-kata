f5fdb2d88639f38643ba161b6c7d887c82025755
Beñat <iribarren.148642@e.unavarra.es>
2025-04-01 13:21:47 +0200

    [#] - Refactorizado ShoppingCart.php para tener un codigo más limpio

--- src/ShoppingCart.php
+++ src/ShoppingCart.php
@@ -11,30 +11,57 @@ class ShoppingCart
             $product = str_replace('añadir ', '', $product);
             $productParts = explode(' ', $product);
 
-            if (!array_key_exists($productParts[0], $this->shoppingList)) {
-                $this->shoppingList[$productParts[0]] = (int)$productParts[1] + 1;
-            } else {
-                $this->shoppingList[$productParts[0]] += (int)$productParts[1] + 1;
-            }
+            $this->annadirCantidadAlProducto($productParts);
         }
 
         else if (strpos($product, 'eliminar') !== false) {
             $product = str_replace('eliminar ', '', $product);
             $productParts = explode(' ', $product);
 
-            if (array_key_exists($productParts[0], $this->shoppingList)) {
+            if ($this->existeProducto($productParts[0])) {
                 unset($this->shoppingList[$productParts[0]]);
             } else{
                 return 'El producto seleccionado no existe';
             }
         }
 
+        $result = $this->prepararListadoDelCarrito();
+
+        return implode(', ', $result);
+    }
+
+    /**
+     * @return array
+     */
+    public function prepararListadoDelCarrito(): array
+    {
         ksort($this->shoppingList);
         $result = [];
         foreach ($this->shoppingList as $item => $quantity) {
             $result[] = $item . ' x' . $quantity;
         }
+        return $result;
+    }
 
-        return implode(', ', $result);
+    /**
+     * @param array $productParts
+     * @return void
+     */
+    public function annadirCantidadAlProducto(array $productParts): void
+    {
+        if (!$this->existeProducto($productParts[0])) {
+            $this->shoppingList[$productParts[0]] = (int)$productParts[1] + 1;
+        } else {
+            $this->shoppingList[$productParts[0]] += (int)$productParts[1] + 1;
+        }
+    }
+
+    /**
+     * @param $key
+     * @return bool
+     */
+    public function existeProducto($key): bool
+    {
+        return array_key_exists($key, $this->shoppingList);
     }
 }
